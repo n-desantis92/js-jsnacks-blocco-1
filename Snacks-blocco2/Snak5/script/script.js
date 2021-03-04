e7d4daaadca35d2aee6fab4b86bef988a6e8b511
n-desantis92 <dsnwebdev@gmail.com>
2021-03-04 15:55:18 +0100

    aggiunto snack 5 blocco 2

--- Snacks-blocco2/Snak5/script/script.js
+++ Snacks-blocco2/Snak5/script/script.js
@@ -2,10 +2,21 @@
 var contenitore1 = ["1","2","3","4","5","6"];
 var contenitore2 = ["1","2","3","4","5","6","7","8","9","10"];
 // Aggiungi elementi casuali all'array che ha meno elementi, fino a quando ne avrà tanti quanti l'altro.
+var numero = 0;
+// for (var i = contenitore1.length; i < contenitore2.length; i++) {
+//   numero = Math.floor(Math.random() * 10 + 1).toString();
+//   contenitore1.push(numero);
+// }
+//
+// console.log(contenitore1);
+
+// altrimenti se voglio prendere una stringa dall'array
 
 for (var i = contenitore1.length; i < contenitore2.length; i++) {
-  var numero = Math.floor(Math.random() * 10 + 1).toString();
-  contenitore1.push(numero);
+  numero = Math.floor(Math.random() * (contenitore2.length));
+  console.log(numero + "numero random");
+  contenitore1.push(contenitore2[numero]);
+  console.log(contenitore2[numero] + " numero contenitore");
 }
 
 console.log(contenitore1);
